b4f43c28e68b5561681f67f91a3a2d504f39f816
ogkunta666 <markizoli06@gmail.com>
2025-11-06 08:02:21 +0100

    elso

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/hello', function (Request $request) {
+    return $response()->json(['message' => 'Hello, API!']);
+})->middleware('auth:sanctum');
+
+?>
